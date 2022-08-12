2deaa6b8534c7b5179dccac7a3061844159d224c
cklewar@t-online.de <cklewar@t-online.de>
2022-08-12 11:49:46 +0200

    update submodule

--- versions.tf
+++ versions.tf
@@ -1,12 +1,16 @@
 terraform {
-  required_version = ">= 1.2.4"
+  required_version = ">= 1.2.7"
+  cloud {
+    organization = "cklewar"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "aws-vpc-module"
+    }
+  }
+
 
   required_providers {
-    volterra = {
-      source  = "volterraedge/volterra"
-      version = ">= 0.11.9"
-    }
-
     local = ">= 2.2.3"
     null  = ">= 3.1.1"
   }
